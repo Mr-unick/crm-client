fc4f35b3e5aeb398548d97cc471ca04b3d506447
Nikhil Lende <110834015+Mr-unick@users.noreply.github.com>
2024-09-17 23:08:33 +0530

    Update admin.js

--- src/services/admin.js
+++ src/services/admin.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const signInAdmin = async (Admindata) => {
   try {
     const response = await axios.post(
-      `https://82.112.231.43:443/admin/signin`,
+      `https://82.112.231.43:400/admin/signin`,
       Admindata
     );
 
